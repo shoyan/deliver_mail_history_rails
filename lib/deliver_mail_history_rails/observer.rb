b8504cdd8aece31de2ba35b84ecf10a1c80caa82
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-01-19 15:11:24 +0900

    Specify model name and column

--- lib/deliver_mail_history_rails/observer.rb
+++ lib/deliver_mail_history_rails/observer.rb
@@ -1,12 +1,13 @@
 module DeliverMailHistoryRails
   class Observer
     def self.delivered_email(mail)
-      DeliverMailHistory.new(
-        mail_to: mail.to,
-        mail_subject: mail.subject,
-        mail_header: mail.header.to_s,
-        mail_body: mail.decode_body,
-        send_date: mail.date.to_s(:db)
+      address = Configuration.address.to_s
+      eval(address).new(
+        Configuration.column(:mail_to) => mail.to,
+        Configuration.column(:mail_subject) => mail.subject,
+        Configuration.column(:mail_header) => mail.header.to_s,
+        Configuration.column(:mail_body) => mail.decode_body,
+        Configuration.column(:send_date) => mail.date.to_s(:db)
       ).save
     end
   end
